8dd2f0d0453d6d613a483ae4d7c7876b0c60e949
Yialeloglou <yia002@csiro.au>
2019-10-03 10:47:48 +1000

    update the initial load script to create the new variable for 1977 only

--- scripts/ggpplot_challenge1.R
+++ scripts/ggpplot_challenge1.R
@@ -1,3 +1,5 @@
 library(tidyverse) # load the tidyverse
 
 gapminder <- read_csv("data/gapminder.csv") # create the gapminder variable from te csv file
+
+gapminder_1977 <- filter(gapminder, year == 1977)
